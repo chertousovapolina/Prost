e3afc9cd4d9b117e2826e10f809cbdfce4412cc4
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-30 15:16:27 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+
+    for (int i = 2; i < a; i++) {
+        if (a % i == 0) { // если число делится на i=2 - оно не простое
+            cout << "No";
+
+            return 0;
+        }
+
+        else
+        {
+            cout << "Yes";
+        }
+        return 0;
+    }
+}
+
+
